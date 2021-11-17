731e6bba914dd01566734fbe0657bc6ad8e9914a
Evert Kors <evert@hatching.io>
2021-11-17 17:40:04 +0100

    add deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,16 @@
-require("@nomiclabs/hardhat-waffle");
-
+require('@nomiclabs/hardhat-waffle');
+require('solidity-coverage');
+require('hardhat-gas-reporter');
+require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: 'USD',
+    gasPrice: 100,
+    coinmarketcap: process.env.COINMARKETCAP,
+  },
 };
