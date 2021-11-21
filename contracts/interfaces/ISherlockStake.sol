66fcdc65b8a5ce5ffeab1b508e117c9c247800b9
John Sanford <jsanford9292@gmail.com>
2021-11-20 22:03:38 -0800

    changing natspec on interfaces

--- contracts/interfaces/ISherlockStake.sol
+++ contracts/interfaces/ISherlockStake.sol
@@ -9,30 +9,30 @@ pragma solidity 0.8.9;
 /// @title Sherlock core interface for stakers
 /// @author Evert Kors
 interface ISherlockStake {
-  /// @notice View the current deadline of `_tokenID`
+  /// @notice View the current lockup end timestamp of `_tokenID`
   /// @return Timestamp when NFT position unlocks
   function lockupEnd(uint256 _tokenID) external view returns (uint256);
 
   /// @notice View the current SHER reward of `_tokenID`
-  /// @return Amount of SHER rewarded to owner upon reaching deadline
+  /// @return Amount of SHER rewarded to owner upon reaching the end of the lockup
   function sherRewards(uint256 _tokenID) external view returns (uint256);
 
-  /// @notice View the current token balance claimable upon reaching deadline
-  /// @return Amount of tokens assigned to owner when burning position
+  /// @notice View the current token balance claimable upon reaching end of the lockup
+  /// @return Amount of tokens assigned to owner when unstaking position
   function tokenBalanceOf(uint256 _tokenID) external view returns (uint256);
 
-  /// @notice View current total staker TVL
+  /// @notice View the current TVL for all stakers
   /// @return Total amount of tokens staked
-  /// @dev Contains principles + strategy + premiums
-  /// @dev Will calculate the most up to date value for each second
+  /// @dev Adds principal + strategy + premiums
+  /// @dev Will calculate the most up to date value for each piece
   function totalTokenBalanceStakers() external view returns (uint256);
 
-  /// @notice Stake `_amount` and lockup for `_period` seconds, `_receiver` will receive the receipt.`
+  /// @notice Stakes `_amount` of tokens and locks up for `_period` seconds, `_receiver` will receive the NFT receipt
   /// @param _amount Amount of tokens to stake
   /// @param _period Period of time, in seconds, to lockup your funds
   /// @param _receiver Address that will receive the NFT representing the position
-  /// @return _id TokenID of the position
-  /// @return _sher Amount of SHER tokens to be relased after `_period` ends
+  /// @return _id ID of the position
+  /// @return _sher Amount of SHER tokens to be released to this ID after `_period` ends
   /// @dev `_period` needs to be whitelisted
   function initialStake(
     uint256 _amount,
@@ -40,29 +40,29 @@ interface ISherlockStake {
     address _receiver
   ) external returns (uint256 _id, uint256 _sher);
 
-  /// @notice Burn `_id` and receive `_amount` tokens
+  /// @notice Redeem NFT `_id` and receive `_amount` of tokens
   /// @param _id TokenID of the position
-  /// @return _amount Amount of tokens released
-  /// @dev Only the owner of `_id` will be able to burn their position
-  /// @dev The remaining SHER rewards will be released
+  /// @return _amount Amount of tokens (USDC) owed to NFT ID
+  /// @dev Only the owner of `_id` will be able to redeem their position
+  /// @dev The SHER rewards are sent to the NFT owner
   /// @dev Can only be called after lockup `_period` has ended
   function redeemNFT(uint256 _id) external returns (uint256 _amount);
 
-  /// @notice Keep holding position with ID: `_id` for `_period` seconds
-  /// @param _id TokenID of the position
+  /// @notice Owner restakes position with ID: `_id` for `_period` seconds
+  /// @param _id ID of the position
   /// @param _period Period of time, in seconds, to lockup your funds
-  /// @return _sher Amount of SHER tokens to be relased after `_period` ends
+  /// @return _sher Amount of SHER tokens to be released to owner address after `_period` ends
   /// @dev Only the owner of `_id` will be able to restake their position using this call
   /// @dev `_period` needs to be whitelisted
   /// @dev Can only be called after lockup `_period` has ended
   function ownerRestake(uint256 _id, uint256 _period) external returns (uint256 _sher);
 
-  /// @notice Force owner to hold position with ID: `_id` for 3 months
-  /// @param _id TokenID of the position
-  /// @return _sher Amount of SHER tokens to be relased to owner after 3 months
-  /// @return _arbReward Amount of tokens sent to caller
-  /// @dev Can only be called after lockup `_period` is more than 2 weeks in the past
-  /// @dev Max 10% of tokens in positions are used to incentivize arbs (x)
+  /// @notice Allows someone who doesn't own the position (an arbitrager) to restake the position for 3 months (ARB_RESTAKE_PERIOD)
+  /// @param _id ID of the position
+  /// @return _sher Amount of SHER tokens to be released to position owner on expiry of the 3 month lockup
+  /// @return _arbReward Amount of tokens (USDC) sent to caller (the arbitrager) in return for calling the function
+  /// @dev Can only be called after lockup `_period` is more than 2 weeks in the past (assuming ARB_RESTAKE_WAIT_TIME is 2 weeks)
+  /// @dev Max 10% (ARB_RESTAKE_MAX_PERCENTAGE) of tokens associated with a position are used to incentivize arbs (x)
   /// @dev During a 2 week period the reward ratio will move from 0% to 100% (* x)
   function arbRestake(uint256 _id) external returns (uint256 _sher, uint256 _arbReward);
 }
--- contracts/interfaces/managers/ISherlockProtocolManager.sol
+++ contracts/interfaces/managers/ISherlockProtocolManager.sol
@@ -51,16 +51,16 @@ interface ISherlockProtocolManager is IManager {
 
   event ProtocolPremiumChanged(bytes32 protocol, uint256 oldPremium, uint256 newPremium);
 
-  /// @notice View current chunk of premium that are claimable
+  /// @notice View current amount of all premiums that are owed to stakers
   /// @return Premiums claimable
-  /// @dev will increase every second
-  /// @dev base + (now - last_settled) * pb
+  /// @dev Will increase every block
+  /// @dev base + (now - last_settled) * ps
   function claimablePremiums() external view returns (uint256);
 
-  /// @notice Transfer current claimable premiums to core address
-  /// @dev callable by everyone
-  /// @dev will be called on burn() from sherlock
-  /// @dev funds will be transferred to sherlock core
+  /// @notice Transfer current claimable premiums (for stakers) to core Sherlock address
+  /// @dev Callable by everyone
+  /// @dev Will be called by burn() in Sherlock core contract
+  /// @dev Funds will be transferred to Sherlock core contract
   function claimPremiumsForStakers() external;
 
   /// @notice View current protocolAgent of `_protocol`
@@ -73,24 +73,24 @@ interface ISherlockProtocolManager is IManager {
   /// @return Amount of premium `_protocol` pays per second
   function premium(bytes32 _protocol) external view returns (uint256);
 
-  /// @notice View current active balance of protocol
+  /// @notice View current active balance of covered protocol
   /// @param _protocol Protocol identifier
   /// @return Active balance
-  /// @dev Accrued debt is subtracted from the stored balance
+  /// @dev Accrued debt is subtracted from the stored active balance
   function activeBalance(bytes32 _protocol) external view returns (uint256);
 
-  /// @notice View seconds of coverage left of `_protocol`
+  /// @notice View seconds of coverage left for `_protocol` before it runs out of active balance
   /// @param _protocol Protocol identifier
   /// @return Seconds of coverage left
   function secondsOfCoverageLeft(bytes32 _protocol) external view returns (uint256);
 
   /// @notice Add a new protocol to Sherlock
   /// @param _protocol Protocol identifier
-  /// @param _protocolAgent Account able to submit a claim on behalve of the protocol
+  /// @param _protocolAgent Address able to submit a claim on behalf of the protocol
   /// @param _coverage Hash referencing the active coverage agreement
-  /// @param _nonStakers Percentage of premium payments that is not redirected to stakers
-  /// @param _coverageAmount Max amount claimable
-  /// @dev Adding a protocol allows the `_protocolAgent` to submit a claim.
+  /// @param _nonStakers Percentage of premium payments to nonstakers, scaled by 10**18
+  /// @param _coverageAmount Max amount claimable by this protocol
+  /// @dev Adding a protocol allows the `_protocolAgent` to submit a claim
   /// @dev Coverage is not started yet as the protocol doesn't pay a premium at this point
   /// @dev `_nonStakers` is scaled by 10**18
   /// @dev Only callable by governance
@@ -105,8 +105,8 @@ interface ISherlockProtocolManager is IManager {
   /// @notice Update info regarding a protocol
   /// @param _protocol Protocol identifier
   /// @param _coverage Hash referencing the active coverage agreement
-  /// @param _nonStakers Percentage of premium payments that is not redirected to stakers, scaled by 10**18
-  /// @param _coverageAmount Max amount claimable
+  /// @param _nonStakers Percentage of premium payments to nonstakers, scaled by 10**18
+  /// @param _coverageAmount Max amount claimable by this protocol
   /// @dev Only callable by governance
   function protocolUpdate(
     bytes32 _protocol,
@@ -115,20 +115,20 @@ interface ISherlockProtocolManager is IManager {
     uint256 _coverageAmount
   ) external;
 
-  /// @notice Remove a protocol
+  /// @notice Remove a protocol from coverage
   /// @param _protocol Protocol identifier
-  /// @dev Before removing a protocol the premium should be 0
-  /// @dev Removing a protocol basically stops the `_protocolAgent` from being active
+  /// @dev Before removing a protocol the premium must be 0
+  /// @dev Removing a protocol basically stops the `_protocolAgent` from being active (can still submit claims until claim deadline though) 
   /// @dev Pays off debt + sends remaining balance to protocol agent
   /// @dev This call should be subject to a timelock
   /// @dev Only callable by governance
   function protocolRemove(bytes32 _protocol) external;
 
-  /// @notice Remove a protocol with insufificient balance
+  /// @notice Remove a protocol with insufficient active balance
   /// @param _protocol Protocol identifier
   function forceRemoveByActiveBalance(bytes32 _protocol) external;
 
-  /// @notice Remove a protocol with insufificient coverage time
+  /// @notice Removes a protocol with insufficent seconds of coverage left
   /// @param _protocol Protocol identifier
   function forceRemoveBySecondsOfCoverage(bytes32 _protocol) external;
 
@@ -140,14 +140,14 @@ interface ISherlockProtocolManager is IManager {
   /// @return Minimal seconds of coverage needed
   function minSecondsOfCoverage() external view returns (uint256);
 
-  /// @notice Set minimal balance needed before liquidation can start
-  /// @param _minBalance Mininal balance needed
-  /// @dev only gov
+  /// @notice Sets the minimum active balance before an arb can remove a protocol
+  /// @param _minActiveBalance Minimum balance needed (in USDC)
+  /// @dev Only gov
   function setMinActiveBalance(uint256 _minBalance) external;
 
-  /// @notice Set minimal seconds of coverage needed before liquidation can start
-  /// @param _minSeconds Mininal seconds of coverage needed
-  /// @dev only gov
+  /// @notice Sets the minimum active balance (as measured in seconds of coverage left) before an arb can remove a protocol
+  /// @param _minSeconds Minimum seconds of coverage needed
+  /// @dev Only gov
   function setMinSecondsOfCoverage(uint256 _minSeconds) external;
 
   /// @notice Set premium of `_protocol` to `_premium`
@@ -158,49 +158,51 @@ interface ISherlockProtocolManager is IManager {
   function setProtocolPremium(bytes32 _protocol, uint256 _premium) external;
 
   /// @notice Set premium of multiple protocols
-  /// @param _protocol Protocol identifier
-  /// @param _premium Amount of premium `_protocol` pays per second
+  /// @param _protocol Array of protocol identifiers
+  /// @param _premium Array of premium amounts protocols pay per second
   /// @dev The value 0 would mean inactive coverage
   /// @dev Only callable by governance
   function setProtocolPremiums(bytes32[] calldata _protocol, uint256[] calldata _premium) external;
 
-  /// @notice Deposit `_amount` token for pay premium for `_protocol`
+  /// @notice Deposits `_amount` of token to the active balance of `_protocol`
   /// @param _protocol Protocol identifier
   /// @param _amount Amount of tokens to deposit
   /// @dev Approval should be made before calling
   function depositToActiveBalance(bytes32 _protocol, uint256 _amount) external;
 
-  /// @notice Withdraw `_amount` token that would pay premium for `_protocol`
+  /// @notice Withdraws `_amount` of token from the active balance of `_protocol`
   /// @param _protocol Protocol identifier
   /// @param _amount Amount of tokens to withdraw
-  /// @dev Only claim starter role is able to withdraw balance
-  /// @dev Balance can be withdraw up until 3 days of coverage outstanding
+  /// @dev Only protocol agent is able to withdraw
+  /// @dev Balance can be withdrawn up until 3 days worth of active balance
   /// @dev In case coverage is not active (0 premium), full balance can be withdrawn
   function withdrawActiveBalance(bytes32 _protocol, uint256 _amount) external;
 
-  /// @notice Transfer claimStarer role
+  /// @notice Transfer protocol agent role
   /// @param _protocol Protocol identifier
-  /// @param _protocolAgent Account able to submit a claim on behalve of the protocol
+  /// @param _protocolAgent Account able to submit a claim on behalf of the protocol
   /// @dev Only the active protocolAgent is able to transfer the role
   function transferProtocolAgent(bytes32 _protocol, address _protocolAgent) external;
 
-  /// @notice View how much the non stakers can claim for this protocol
+  /// @notice View the amount nonstakers can claim from this protocol
   /// @param _protocol Protocol identifier
-  /// @return Amount of token claimable by non stakers
-  /// @dev this read from a storage variable + (now-lastsettled) * premiums
+  /// @return Amount of tokens claimable by nonstakers
+  /// @dev this reads from a storage variable + (now-lastsettled) * premiums
   function nonStakersClaimable(bytes32 _protocol) external view returns (uint256);
 
-  /// @notice Send `_amount` tokens to `_receiver` that non staker can claim from `_protocol`
+  /// @notice Choose an `_amount` of tokens that nonstakers (`_receiver` address) will receive from `_protocol`
   /// @param _protocol Protocol identifier
   /// @param _amount Amount of tokens
   /// @param _receiver Address to receive tokens
-  /// @dev Only callable by non stakers role
+  /// @dev Only callable by nonstakers role
   function nonStakersClaim(
     bytes32 _protocol,
     uint256 _amount,
     address _receiver
   ) external;
 
+  /// @param _protocol Protocol identifier
+  /// @return current and previous are the current and previous coverage amounts for this protocol
   function coverageAmounts(bytes32 _protocol)
     external
     view
