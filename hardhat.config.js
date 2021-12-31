1499e60fcd82c6b1900c2cae22d4d81c1569b44f
Evert Kors <evert@hatching.io>
2021-12-31 11:43:24 +0100

    Initial script

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const ALCHEMY_API_KEY_MAINNET = process.env.ALCHEMY_API_KEY_MAINNET || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+
 module.exports = {
   solidity: {
     version: '0.8.10',
@@ -22,6 +26,21 @@ module.exports = {
         mnemonic:
           'apart turn peace asthma useful mother tank math engine usage prefer orphan exile fold squirrel',
       },
+    },
+    localhost: {
+      timeout: 999999999,
+      gasPrice: 1600000000000,
+      //accounts: [PRIVATE_KEY].filter(item => item !== ''),
+    },
+    local: {
+      url: 'http://127.0.0.1:8545',
+      gasPrice: 50000000000,
+    },
+    mainnet: {
+      timeout: 999999999,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY_MAINNET}`,
+      gasPrice: 100000000000,
+      accounts: [PRIVATE_KEY].filter((item) => item !== ''),
     },
   },
   gasReporter: {
