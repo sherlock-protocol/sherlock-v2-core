5a0536a5a7fd2dc9989f18e9b425d2870b9f89b8
Evert Kors <evert@hatching.io>
2022-01-13 21:03:22 -0500

    Add comments and simplify names

--- contracts/BuySher.sol
+++ contracts/BuySher.sol
@@ -14,23 +14,31 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 /// @title Sherlock core interface for stakers
 /// @author Evert Kors
-// This is the contract that manages staking actions
-
+// This is the contract that manages the buying of SHER tokens
+// Tokens can be bought on a fixed rate
+// Tokens can only be bought if the sender is owner of a sherlock staking position
+// For every staking positon the sender has, they are able to buy tokens once
+// The max amount they are able to buy is based on the underlying USDC value of the positions
+// On every buy USDC is send to the owner, which will be multisig
 contract BuySher is Ownable {
   using SafeERC20 for IERC20;
 
+  // Amount of decimals USDC has
   uint256 internal constant USDC_DECIMALS = 10**6;
+
   uint256 internal constant SHER_DECIMALS = 10**18;
   // 200 SHER tokens
   uint256 public constant SHER_DUST_AMOUNT = 200 * 10**18;
 
   IERC20 public immutable sher;
   IERC20 public immutable usdc;
+  // 'rate' being 10**18 means 1 USDC of the position is able to buy 1 SHER token
   uint256 public immutable rate;
+  // 'cost' being 10**6 means 1 USDC transferred will buy 1 SHER token
   uint256 public immutable cost;
   ISherlock public immutable sherlockPosition;
 
-  mapping(uint256 => bool) sherlockPositionUsed;
+  mapping(uint256 => bool) public sherlockPositionUsed;
 
   error InvalidSender();
   error AlreadyUsed();
@@ -45,20 +53,24 @@ contract BuySher is Ownable {
     uint256 _cost,
     ISherlock _sherlockPosition
   ) {
+    if (address(_sher) == address(0)) revert ZeroArgument();
+    if (address(_usdc) == address(0)) revert ZeroArgument();
+    if (_rate == 0) revert ZeroArgument();
+    if (_cost == 0) revert ZeroArgument();
+    if (address(_sherlockPosition) == address(0)) revert ZeroArgument();
+
     sher = _sher;
     usdc = _usdc;
-    // _rate being 10**18 means 1 USDC staked is able to buy 1 SHER token
     rate = _rate;
-    // _cost being 10**6 means 1 USDC transferred will buy 1 SHER token
     cost = _cost;
     sherlockPosition = _sherlockPosition;
   }
 
-  function calcMaxSherPurchase(uint256 _sherlockPositionID) public view returns (uint256) {
+  function viewBuyLimit(uint256 _sherlockPositionID) public view returns (uint256) {
     if (sherlockPositionUsed[_sherlockPositionID]) revert AlreadyUsed();
 
     // It is known that the underlying usdc amount will be higher as the aave/premiums come in
-    // Potentially increasing the purchase power
+    // Potentially increasing the buy power
     uint256 usdcBalance = sherlockPosition.tokenBalanceOf(_sherlockPositionID);
     uint256 maxSherAmount = (usdcBalance * rate) / USDC_DECIMALS;
     uint256 contractSherBalance = sher.balanceOf(address(this));
@@ -66,19 +78,21 @@ contract BuySher is Ownable {
     return (contractSherBalance > maxSherAmount) ? contractSherBalance : maxSherAmount;
   }
 
-  function calcSherPurchaseCosts(uint256 _amountOfSher) public view returns (uint256) {
+  function viewCosts(uint256 _amountOfSher) public view returns (uint256) {
     return (_amountOfSher * cost) / SHER_DECIMALS;
   }
 
-  function purchase(uint256 _sherlockPositionID, uint256 _amountOfSher) external {
+  function buy(uint256 _sherlockPositionID, uint256 _amountOfSher) external {
+    if (_sherlockPositionID == 0) revert ZeroArgument();
+    if (_amountOfSher == 0) revert ZeroArgument();
     if (msg.sender != sherlockPosition.ownerOf(_sherlockPositionID)) revert InvalidSender();
 
-    // calcMaxSherPurchase will revert is sherlockPositionUsed = true
-    if (_amountOfSher > calcMaxSherPurchase(_sherlockPositionID)) revert InvalidAmount();
+    // viewBuyLimit will revert is sherlockPositionUsed = true
+    if (_amountOfSher > viewBuyLimit(_sherlockPositionID)) revert InvalidAmount();
     sherlockPositionUsed[_sherlockPositionID] = true;
 
     // USDC will be send to owner, which is a multisig
-    usdc.safeTransferFrom(msg.sender, owner(), calcSherPurchaseCosts(_amountOfSher));
+    usdc.safeTransferFrom(msg.sender, owner(), viewCosts(_amountOfSher));
     sher.safeTransfer(msg.sender, _amountOfSher);
   }
 
