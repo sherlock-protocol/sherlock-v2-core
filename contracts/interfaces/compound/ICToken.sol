cca4a12027b4e35df958ebf557af64f5a0c10006
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-04-08 11:33:36 -0300

    feat: cToken <-> token exchange rate

--- contracts/interfaces/compound/ICToken.sol
+++ contracts/interfaces/compound/ICToken.sol
@@ -8,13 +8,43 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
  */
 
 interface ICToken is IERC20 {
-  function mint(uint256) external returns (uint256);
+  function mint(uint256) external virtual returns (uint256);
 
-  function exchangeRateCurrent() external returns (uint256);
+  function borrow(uint256) external virtual returns (uint256);
 
-  function supplyRatePerBlock() external returns (uint256);
+  function underlying() external view virtual returns (ERC20);
 
-  function redeem(uint256) external returns (uint256);
+  function totalBorrows() external view virtual returns (uint256);
 
-  function redeemUnderlying(uint256) external returns (uint256);
+  function totalFuseFees() external view virtual returns (uint256);
+
+  function repayBorrow(uint256) external virtual returns (uint256);
+
+  function totalReserves() external view virtual returns (uint256);
+
+  function exchangeRateCurrent() external virtual returns (uint256);
+
+  function totalAdminFees() external view virtual returns (uint256);
+
+  function fuseFeeMantissa() external view virtual returns (uint256);
+
+  function adminFeeMantissa() external view virtual returns (uint256);
+
+  function exchangeRateStored() external view virtual returns (uint256);
+
+  function accrualBlockNumber() external view virtual returns (uint256);
+
+  function redeemUnderlying(uint256) external virtual returns (uint256);
+
+  function balanceOfUnderlying(address) external virtual returns (uint256);
+
+  function reserveFactorMantissa() external view virtual returns (uint256);
+
+  function borrowBalanceCurrent(address) external virtual returns (uint256);
+
+  function interestRateModel() external view virtual returns (InterestRateModel);
+
+  function initialExchangeRateMantissa() external view virtual returns (uint256);
+
+  function repayBorrowBehalf(address, uint256) external virtual returns (uint256);
 }
--- contracts/strategy/CompundStrategy.sol
+++ contracts/strategy/CompundStrategy.sol
@@ -8,6 +8,7 @@ pragma solidity 0.8.10;
 
 import './base/BaseStrategy.sol';
 import '../interfaces/compound/ICToken.sol';
+import { FixedPointMathLib } from 'solmate/utils/FixedPointMathLib.sol';
 
 /**
  * This contract implements the logic to deposit and withdraw
@@ -17,6 +18,7 @@ import '../interfaces/compound/ICToken.sol';
 
 contract CompoundStrategy is BaseStrategy {
   using SafeERC20 for IERC20;
+  using FixedPointMathLib for uint256;
 
   // This is the receipt token Compound gives in exchange for a token deposit (cUSDC)
   // @see https://compound.finance/docs#protocol-math
@@ -33,14 +35,15 @@ contract CompoundStrategy is BaseStrategy {
   }
 
   /**
-   * Returns the contract's cWant balance.
+   * @notice Return the contract's want(USDC) balance.
+   * note exchange rate isn't 1:1 between cToken and underlying asset.
    */
   function balanceOf() public view override returns (uint256) {
-    return cWant.balanceOf(address(this));
+    return cWant.balanceOf(address(this)).mulWadDown(_viewExchangeRate());
   }
 
   /**
-   * Deposits the entire contract's balance into Compound.
+   * @notice Deposit the entire contract's balance into Compound.
    */
   function _deposit() internal override whenNotPaused {
     uint256 amount = want.balanceOf(address(this));
@@ -54,7 +57,7 @@ contract CompoundStrategy is BaseStrategy {
   }
 
   /**
-   * Withdraws the entire cWant balance from Compound.
+   * @notice Withdraw the entire cWant balance from Compound.
    * @see _withdraw(uint256).
    */
   function _withdrawAll() internal override returns (uint256) {
@@ -64,7 +67,7 @@ contract CompoundStrategy is BaseStrategy {
   }
 
   /**
-   * Withdraws a specific cWant amount from Compound.
+   * @notice Withdraw a specific cWant amount from Compound.
    */
   function _withdraw(uint256 amount) internal override returns (uint256) {
     if (amount == 0) {
@@ -74,5 +77,40 @@ contract CompoundStrategy is BaseStrategy {
     if (cWant.redeem(amount) != 0) revert InvalidState();
 
     return amount;
+  }
+
+  /**
+   * @notice Calculate cToken to underlying asset exchange rate without mutating state.
+   * note based on transmissions11's lib (https://github.com/transmissions11/libcompound)
+   */
+  function _viewExchangeRate() internal view returns (uint256) {
+    uint256 accrualBlockNumberPrior = cWant.accrualBlockNumber();
+
+    if (accrualBlockNumberPrior == block.number) return cWant.exchangeRateStored();
+
+    uint256 totalCash = want.balanceOf(address(cWant));
+    uint256 borrowsPrior = cWant.totalBorrows();
+    uint256 reservesPrior = cWant.totalReserves();
+
+    uint256 borrowRateMantissa = cWant.interestRateModel().getBorrowRate(
+      totalCash,
+      borrowsPrior,
+      reservesPrior
+    );
+
+    if (borrowRateMantissa <= 0.0005e16) revert InvalidState();
+
+    uint256 interestAccumulated = (borrowRateMantissa * (block.number - accrualBlockNumberPrior))
+      .mulWadDown(borrowsPrior);
+
+    uint256 totalReserves = cWant.reserveFactorMantissa().mulWadDown(interestAccumulated) +
+      reservesPrior;
+    uint256 totalBorrows = interestAccumulated + borrowsPrior;
+    uint256 totalSupply = cWant.totalSupply();
+
+    return
+      totalSupply == 0
+        ? cWant.initialExchangeRateMantissa()
+        : (totalCash + totalBorrows - totalReserves).divWadDown(totalSupply);
   }
 }
